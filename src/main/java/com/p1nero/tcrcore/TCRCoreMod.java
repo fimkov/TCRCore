dc2125136c713112564b59a9c362bcba4bf1247e
P1nero <1070402601@qq.com>
2026-02-11 12:48:49 +0800

    把中文翻译独立出一个资源包

--- src/main/java/com/p1nero/tcrcore/TCRCoreMod.java
+++ src/main/java/com/p1nero/tcrcore/TCRCoreMod.java
@@ -125,6 +125,13 @@ public class TCRCoreMod {
 
     private void addPackFindersEvent(AddPackFindersEvent event) {
         if (event.getPackType() == PackType.CLIENT_RESOURCES) {
+            String name = "zh_cn_translation";
+            var resourcePath = ModList.get().getModFileById(MOD_ID).getFile().findResource("packs/" + name);
+            var pack = Pack.readMetaAndCreate(name, Component.literal("远梦之棺中文包"), true,
+                    (path) -> new PathPackResources(path, resourcePath, false), PackType.CLIENT_RESOURCES, Pack.Position.TOP, PackSource.BUILT_IN);
+            event.addRepositorySource((packConsumer) -> packConsumer.accept(pack));
+        }
+        if (event.getPackType() == PackType.CLIENT_RESOURCES) {
             String name = "tcr_assets";
             var resourcePath = ModList.get().getModFileById(MOD_ID).getFile().findResource("packs/" + name);
             var pack = Pack.readMetaAndCreate(name, Component.literal("The Casket of Reveries Assets - Override"), true,
