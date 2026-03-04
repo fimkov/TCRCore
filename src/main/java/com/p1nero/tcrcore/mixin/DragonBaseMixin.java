7ce5d0c2c6bbca0c6580694b2acf8744a1200f08
p1nero <1070402601@qq.com>
2026-03-04 23:24:04 +0800

    暗改龙的调试

--- src/main/java/com/p1nero/tcrcore/mixin/DragonBaseMixin.java
+++ src/main/java/com/p1nero/tcrcore/mixin/DragonBaseMixin.java
@@ -2,17 +2,23 @@ package com.p1nero.tcrcore.mixin;
 
 import com.p1nero.tcrcore.capability.TCRQuestManager;
 import com.p1nero.tcrcore.capability.TCRQuests;
+import com.p1nero.tcrcore.item.TCRItems;
 import net.magister.bookofdragons.entity.base.dragon.DragonBase;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.ContainerListener;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.TamableAnimal;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import software.bernie.geckolib.animatable.GeoEntity;
 
 @Mixin(DragonBase.class)
@@ -28,15 +34,18 @@ public abstract class DragonBaseMixin extends TamableAnimal implements GeoEntity
         super(p_21803_, p_21804_);
     }
 
-//    /**
-//     * 调试养大用 TODO 记得删了
-//     */
-//    @Inject(method = "tick", at = @At("TAIL"))
-//    private void tcr$tick(CallbackInfo ci) {
-//        if(!FMLEnvironment.production) {
-//            this.setGrowthProgress(this.getGrowthProgress() + 6000);
-//        }
-//    }
+    /**
+     * 调试养大用
+     */
+    @Inject(method = "mobInteract", at = @At("TAIL"))
+    private void tcr$mobInteract(Player pPlayer, InteractionHand pHand, CallbackInfoReturnable<InteractionResult> cir) {
+        if(pPlayer.getMainHandItem().is(TCRItems.DIVINE_FRAGMENT.get())) {
+            this.setGrowthProgress(this.getGrowthProgress() + 6000);
+            if(!pPlayer.isLocalPlayer()) {
+                pPlayer.getMainHandItem().shrink(1);
+            }
+        }
+    }
 
     /**
      * 龙养大后
