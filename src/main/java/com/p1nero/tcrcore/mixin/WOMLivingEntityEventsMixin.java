502e3c5e1c4c6f2778bdea752b01dd22aedfbb5c
p1nero <1070402601@qq.com>
2026-03-07 23:29:33 +0800

    更新title配置，wom加保险移除，弱化小怪

--- src/main/java/com/p1nero/tcrcore/mixin/WOMLivingEntityEventsMixin.java
+++ src/main/java/com/p1nero/tcrcore/mixin/WOMLivingEntityEventsMixin.java
@@ -7,7 +7,10 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import reascer.wom.animation.attacks.UltimateAttackAnimation;
 import reascer.wom.events.WOMLivingEntityEvents;
+import yesman.epicfight.world.capabilities.EpicFightCapabilities;
+import yesman.epicfight.world.capabilities.entitypatch.LivingEntityPatch;
 
 import static reascer.wom.events.WOMLivingEntityEvents.*;
 
@@ -19,10 +22,25 @@ public class WOMLivingEntityEventsMixin {
         ci.cancel();
     }
 
+    /**
+     * 加非玩家的判断，防止bug
+     */
     @Inject(method = "onUpdateEvent", at = @At("HEAD"), cancellable = true, remap = false)
     private static void tcr$onLivingTick(LivingEvent.LivingTickEvent event, CallbackInfo ci) {
         ci.cancel();
         Entity e = event.getEntity();
+        if(e.isInvulnerable() && !e.level().isClientSide) {
+            LivingEntityPatch<?> entityPatch = EpicFightCapabilities.getEntityPatch(e, LivingEntityPatch.class);
+            if (entityPatch != null && !(entityPatch.getServerAnimator().animationPlayer.getAnimation() instanceof UltimateAttackAnimation)) {
+                for(String tag : event.getEntity().getTags()) {
+                    if (tag.contains("wom_ultimate_Invulnerable")) {
+                        e.setInvulnerable(false);
+                        event.getEntity().getTags().remove(tag);
+                        break;
+                    }
+                }
+            }
+        }
         AntiStunlock(event, e);
         TimedSakuraSlashes(event, e);
         LunarEclipse(event, e);
