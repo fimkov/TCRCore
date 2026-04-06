9d80461519fa5a87d3a99a4e8118d209fe5a9a7e
p1nero <1070402601@qq.com>
2026-04-06 22:47:26 +0800

    ftb直接思索技能书

---
+++ src/main/java/com/p1nero/tcrcore/mixin/ftbq/QuestScreenMixin.java
@@ -0,0 +1,36 @@
+package com.p1nero.tcrcore.mixin.ftbq;
+
+import dev.ftb.mods.ftblibrary.icon.ItemIcon;
+import dev.ftb.mods.ftblibrary.util.TooltipList;
+import dev.ftb.mods.ftbquests.client.gui.quests.QuestScreen;
+import dev.ftb.mods.ftbquests.quest.QuestObjectBase;
+import net.minecraft.world.item.ItemStack;
+import org.arc.efp.compat.EpicSkillsPonderCompat;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import yesman.epicfight.skill.Skill;
+import yesman.epicfight.world.item.EpicFightItems;
+import yesman.epicfight.world.item.SkillBookItem;
+
+@Mixin(QuestScreen.class)
+public class QuestScreenMixin {
+
+    @Inject(method = "addInfoTooltip", at = @At("HEAD"), remap = false)
+    private void tcr$addInfoTooltip(TooltipList list, QuestObjectBase object, CallbackInfo ci) {
+        if(object.getIcon() instanceof ItemIcon icon) {
+            ItemStack stack = icon.getStack();
+            if(stack.is(EpicFightItems.SKILLBOOK.get())) {
+                Skill skill = SkillBookItem.getContainSkill(stack);
+                if(skill != null) {
+                    if(EpicSkillsPonderCompat.hasPonderScene(skill)) {
+                        EpicSkillsPonderCompat.setHoveredSkill(skill);
+                        list.add(EpicSkillsPonderCompat.getPonderTooltip());
+                    }
+                }
+            }
+        }
+    }
+
+}
