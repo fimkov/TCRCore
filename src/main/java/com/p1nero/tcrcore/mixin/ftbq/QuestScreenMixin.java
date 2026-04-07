04ce550c643ffbbced47399f1f4fd9564ae8b4b1
p1nero <1070402601@qq.com>
2026-04-08 00:08:54 +0800

    更新刀光和efp

--- src/main/java/com/p1nero/tcrcore/mixin/ftbq/QuestScreenMixin.java
+++ src/main/java/com/p1nero/tcrcore/mixin/ftbq/QuestScreenMixin.java
@@ -5,7 +5,8 @@ import dev.ftb.mods.ftblibrary.util.TooltipList;
 import dev.ftb.mods.ftbquests.client.gui.quests.QuestScreen;
 import dev.ftb.mods.ftbquests.quest.QuestObjectBase;
 import net.minecraft.world.item.ItemStack;
-import org.arc.efp.compat.EpicSkillsPonderCompat;
+import org.arc.epic_ponder.client.ponder.EFPPonderPlugin;
+import org.arc.epic_ponder.compat.EFMSkillsPonderManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -21,13 +22,22 @@ public class QuestScreenMixin {
     private void tcr$addInfoTooltip(TooltipList list, QuestObjectBase object, CallbackInfo ci) {
         if(object.getIcon() instanceof ItemIcon icon) {
             ItemStack stack = icon.getStack();
+            if(!EFMSkillsPonderManager.hasPonderScene(stack)) {
+                return;
+            }
             if(stack.is(EpicFightItems.SKILLBOOK.get())) {
                 Skill skill = SkillBookItem.getContainSkill(stack);
                 if(skill != null) {
-                    if(EpicSkillsPonderCompat.hasPonderScene(skill)) {
-                        EpicSkillsPonderCompat.setHoveredSkill(skill);
-                        list.add(EpicSkillsPonderCompat.getPonderTooltip());
+                    if(EFMSkillsPonderManager.hasPonderScene(skill)) {
+                        EFMSkillsPonderManager.setHoveredSkill(skill);
+                        list.add(EFMSkillsPonderManager.getPonderTooltip());
                     }
+                }
+            } else {
+                String id = EFPPonderPlugin.getWeaponPresetId(stack);
+                if(id != null) {
+                    EFMSkillsPonderManager.setHoveredWeapon(id);
+                    list.add(EFMSkillsPonderManager.getPonderTooltip());
                 }
             }
         }
